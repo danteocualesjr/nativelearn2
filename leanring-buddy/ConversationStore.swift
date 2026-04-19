10260ed073b11f642591eadd1ea91b2b25e318a9
Dante O. Cuales, Jr. <danteocualesjr@gmail.com>
2026-04-19 22:58:00 +0800

    ui tweak

--- leanring-buddy/ConversationStore.swift
+++ leanring-buddy/ConversationStore.swift
@@ -14,6 +14,7 @@ import Foundation
 // MARK: - Tool Type
 
 enum ConversationToolType: String, Codable, CaseIterable {
+    
     case mobileApp = "mobile_app"
     case webApp = "web_app"
     case internalTool = "internal_tool"
